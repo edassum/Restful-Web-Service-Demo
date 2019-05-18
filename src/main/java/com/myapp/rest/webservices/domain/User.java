8ec095129d705c8be7598189d93e52e8fdb62df2
SUMIT DAS <sumitdas.00117@gmail.com>
2019-05-18 23:58:54 +0530

    added new classes and files

--- src/main/java/com/myapp/rest/webservices/domain/User.java
+++ src/main/java/com/myapp/rest/webservices/domain/User.java
@@ -1,10 +1,12 @@
 package com.myapp.rest.webservices.domain;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -20,6 +22,17 @@ public class User {
 
 	@Past
 	private Date birthDate;
+
+	@OneToMany(mappedBy="user")
+	private List<PostV1> posts;
+
+	public List<PostV1> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<PostV1> posts) {
+		this.posts = posts;
+	}
 
 	public User() {
 	}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,12 @@
-insert into user values(1,sysdate(),'AB');
-insert into user values(2,sysdate(),'SACH');
-insert into user values(3,sysdate(),'RAHUL');
-insert into user values(4,sysdate(),'GANGULY');
+insert into user values(10001,sysdate(),'AB');
+insert into user values(10002,sysdate(),'SACH');
+insert into user values(10003,sysdate(),'RAHUL');
+insert into user values(10004,sysdate(),'GANGULY');
+
+insert into postv1 values(11001,'My first post',10001);
+insert into postv1 values(11002,'My second post',10002);
+insert into postv1 values(11003,'My third post',10001);
+insert into postv1 values(11004,'My fourth post',10003);
+insert into postv1 values(11005,'My fifth post',10003);
+insert into postv1 values(11006,'My sixth post',10001);
+insert into postv1 values(11007,'My seventn post',10004);
